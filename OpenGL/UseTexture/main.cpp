fbf6fd56c6e0a368a915aa3120ee5692bb2d2d33
Reve4Mevol <2664806516@qq.com>
2021-05-11 21:27:03 +0800

    实现纹理的组合渲染，通过mix函数控制两个纹理的着色深度，两个纹理时候，需要使用纹理单元，通过管理glUniform1i将纹理ID设置到对于的GLSL中用到的纹理。

--- OpenGL/UseTexture/main.cpp
+++ OpenGL/UseTexture/main.cpp
@@ -15,7 +15,7 @@ int main() {
     glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwInitHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwInitHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow *window = glfwCreateWindow(800,600,"TextureTest",NULL,NULL);
+    GLFWwindow *window = glfwCreateWindow(800,600,"UseTexture",NULL,NULL);
     if(!window)
     {
         printf("init failed!");
@@ -31,25 +31,22 @@ int main() {
     }
 
     float vertices[] = {
-//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
-            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
-            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
-            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
-            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+//     ---- 位置 ----
+            0.5f,  0.5f, 0.0f,  1.0f, 1.0f,   // 右上
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f,   // 右下
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // 左下
+            -0.5f,  0.5f, 0.0f,   0.0f, 1.0f    // 左上
     };
 
     // 顶点着色器硬编码 vec3数据类型，in关键字，查看编译错误信息
     const char *vertexShaderSrc = "#version 330 core\n"
                                   "layout (location = 0) in vec3 aPos;\n"
-                                  "layout (location = 1) in vec3 aColor;\n"
-                                  "layout (location = 2) in vec2 st;\n"
-                                  "out vec3 outcolor;\n"
+                                  "layout (location = 1) in vec2 st;\n"
                                   "out vec2 outst;\n"
                                   "void main()\n"
                                   "{\n"
-                                  "    gl_Position = vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
-                                  "    outcolor = aColor;\n"
-                                  "    outst = st;\n"
+                                  "    gl_Position = vec4(aPos,1.0);\n"
+                                  "    outst = vec2(st.x,1-st.y);\n"
                                   "}\0";
     unsigned int vertexShaderFd;
     vertexShaderFd = glCreateShader(GL_VERTEX_SHADER);
@@ -64,13 +61,13 @@ int main() {
         std::cout<<"ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"<<infoLog<<std::endl;
     }
     const char *fragmentShaderSrc = "#version 330 core\n"
-                                    "in vec3 outcolor;\n"
                                     "in vec2 outst;\n"
                                     "out vec4 FragColor;\n"
-                                    "uniform sampler2D ourTexture;\n"
+                                    "uniform sampler2D firstTexture;\n"
+                                    "uniform sampler2D secondTexture;\n"
                                     "void main()\n"
                                     "{\n"
-                                    "    FragColor = texture(ourTexture, outst)* vec4(outcolor, 1.0);\n"
+                                    "    FragColor = mix(texture(firstTexture, outst),texture(secondTexture, outst),0.7);\n"
                                     "}\0";
     unsigned int fragmentShaderFd;
     fragmentShaderFd = glCreateShader(GL_FRAGMENT_SHADER);
@@ -109,12 +106,10 @@ int main() {
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,8*sizeof(float),(void *)(0 * sizeof(float)));
-    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,8*sizeof(float),(void *)(3 * sizeof(float)));
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,5*sizeof(float),(void *)(0 * sizeof(float)));
+    glVertexAttribPointer(1,2,GL_FLOAT,GL_FALSE,5*sizeof(float),(void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
-    glEnableVertexAttribArray(2);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -135,9 +130,34 @@ int main() {
     }
     else
     {
-        printf("sdfasdf\n");
+        printf("invalid data\n");
     }
     stbi_image_free(data);
+
+    unsigned char *data2 = stbi_load("./container.jpg",&width,&height,&nrChannels,0);
+    unsigned int texture2;
+    glGenTextures(1,&texture2);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_R,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glBindTexture(GL_TEXTURE_2D,texture2);
+    if(data2)
+    {
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data2);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        printf("invalid data\n");
+    }
+    stbi_image_free(data2);
+
+    glUseProgram(shaderProgram);
+    int pos = glGetUniformLocation(shaderProgram,"firstTexture");
+    glUniform1i(pos,0);
+    int pos2 = glGetUniformLocation(shaderProgram,"secondTexture");
+    glUniform1i(pos,1);
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -154,9 +174,16 @@ int main() {
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D,texture);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D,texture2);
         // draw our first triangle
         glUseProgram(shaderProgram);
+        int pos = glGetUniformLocation(shaderProgram,"firstTexture");
+        glUniform1i(pos,0);
+        int pos2 = glGetUniformLocation(shaderProgram,"secondTexture");
+        glUniform1i(pos,1);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
