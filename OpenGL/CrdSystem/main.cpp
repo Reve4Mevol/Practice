551c9a18b7fb284639d30b17fa4b960fa59f9d46
Reve4Mevol <2664806516@qq.com>
2021-05-18 23:07:50 +0800

    增加glm库的引入

--- OpenGL/CrdSystem/main.cpp
+++ OpenGL/CrdSystem/main.cpp
@@ -3,6 +3,10 @@
 #include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
 
 void processInput(GLFWwindow *window)
 {
